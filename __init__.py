8f946b1d0117340c467ff5c2d7c1649d27eadd99
Josh Higham <josh@higham.com>
2018-12-09 14:28:53 -0700

    Added a function to compile everything when we first run everything. Make initializtion super slow, but Runtime fast.

--- __init__.py
+++ __init__.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+import py_compile as pyc
+import os
+import fnmatch
+
+listOfFiles = os.listdir('.')
+pattern = "*.py"
+for entry in listOfFiles:
+    if fnmatch.fnmatch(entry, pattern):
+            pyc.compile(entry)
