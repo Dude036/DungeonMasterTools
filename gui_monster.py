2117b3de69414688aa4bdb5de65487f2d3d53a81
Josh Higham <josh@higham.com>
2020-09-22 16:49:56 -0600

    implementing a search/autofill feature

--- gui_monster.py
+++ gui_monster.py
@@ -1,20 +1,44 @@
 import eel
 import simplejson as json
 
-def autofill_text():
-    pass
+
+Names = []
+
+# @eel.expose
+def autofill_text(text: str):
+    global Names
+    p = len(text)
+    s = 0
+    e = len(Names) - 1
+    # Binary Search based on starts with
+    while e > s:
+        m = (s + e) // 2
+        print('Name  ', Names[m])
+        if Names[m][:p].lower() < text.lower():
+            s = m + 1
+        elif Names[m][:p].lower() > text.lower():
+            e = m + 1
+        else:
+            print("Breaking")
+            break
+
+    # Validate if there's a reason to continue looking
+    if s == e:
+        print("No suggestion")
+        return []
+    
+    print('Start ', s)
+    print('Middle', m)
+    print('End   ', e)
+    return []
 
 
 if __name__ == '__main__':
-    b_names = json.load(open("beasts.json", 'r'))
-    p_names = json.load(open("pokemon.json", 'r'))    
-    
-    with open("names.txt", 'w') as outf:
-        for name in list(b_names.keys()):
-            outf.write(name + "\n")
-        for name in list(p_names.keys()):
-            outf.write(name + "\n")
+    Names = list(json.load(open("pokemon.json", 'r')).keys())
+    Names.extend(list(json.load(open("beasts.json", 'r')).keys()))
+    Names.sort()
 
+    autofill_text('abra')
 
     # # Set web files folder
     # eel.init('web')
