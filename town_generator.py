13535d0c3f2df8b40986c046f53efa4e38fd2760
Joshua Higham <josh@higham.com>
2020-09-25 18:20:29 -0600

    Formatting Fixes

--- town_generator.py
+++ town_generator.py
@@ -8,28 +8,27 @@ from variance import create_variance
 import simplejson as json
 from masterwork import find_masterwork_traits_weapon, find_masterwork_traits_armor
 
-townHTML = '<!DOCTYPE html><html><head><meta name="viewport" content="width=device-width" /><title></title><style>' \
-           'body{max-width:800px;margin-left:auto;margin-right:auto;padding-left:5px;padding-right:5px;} html{' \
-           'font-family:Arial;}h1, h2 {color:black;text-align:center;} .center{text-align:center;} .bold{font-weight:' \
-           'bold;}.emp{font-style:italic;} table{border:1px solid black;border-spacing:0px;}table tr th {' \
-           'background-color:gray;color:white;padding:5px;}table tr td {margin:0px;padding:5px;}.text-xs{font-size:' \
-           '12px;}.text-sm{font-size:14px;}.text-md{font-size:18px;}.text-lg{font-size:24px;}.text-xl{font-size:' \
-           '32px;}.col-1-3{width:33.3%;float: left;}.col-2-3{width:50%;float:left;}.col-3-3{width:100%;float:left;}' \
-           '.col-1-2{width:50%;float:left;}.col-2-2{width:100%;float:left;}.col-1-4{width:25%;float:left;}.col-2-4{' \
-           'width:33.3%;float:left;}.col-3-4{width:50%;float:left;}.col-4-4{width:100%;float:left;}</style><style ' \
-           'type="text/css">.inventory-table td{border-bottom:1px solid black;}.wrapper-box{width:100%;border:2px ' \
-           'solid black;padding:5px;}</style></head><body><script>function show_hide(ident){\nvar a = document.' \
-           'getElementById(ident);\nif (a.style.display === \'none\'){\na.style.display = \'block\';} else {' \
-           'a.style.display = \'none\';}}</script>'
+townHTML = '<!DOCTYPE html><html><head><meta name="viewport" content="width=device-width" /><title></title><style>body'\
+           '{max-width:800px;margin-left:auto;margin-right:auto;padding-left:5px;padding-right:5px;} html{font-family:'\
+           'Arial;}h1, h2 {color:black;text-align:center;} .center{text-align:center;} .bold{font-weight:bold;} .emp{f'\
+           'ont-style:italic;} table{border:1px solid black;border-spacing:0px;} table tr th {background-color:gray;co'\
+           'lor:white;padding:5px;} table tr td {margin:0px;padding:5px;} .text-xs{font-size:12px;} .text-sm{font-size'\
+           ':14px;} .text-md{font-size:18px;}.text-lg{font-size:24px;} .text-xl{font-size:32px;} .col-1-3{width:33.3%;'\
+           'float: left;} .col-2-3{width:50%;float:left;} .col-3-3{width:100%;float:left;} .col-1-2{width:50%;float:le'\
+           'ft;} .col-2-2{width:100%;float:left;} .col-1-4{width:25%;float:left;} .col-2-4{width:33.3%;float:left;} .c'\
+           'ol-3-4{width:50%;float:left;} .col-4-4{width:100%;float:left;} .inventory-table td{border-bottom:1px solid'\
+           ' black;}.wrapper-box{width:100%;border:2px solid black;padding:5px;}</style></head><body><script>function '\
+           'show_hide(ident){\nvar a = document.getElementById(ident);\nif (a.style.display === \'none\'){\na.style.di'\
+           'splay = \'block\';} else {a.style.display = \'none\';}}</script>'
 
 store_head = '<div class="wrapper-box" style="margin-bottom:60px;"><span class="text-lg bold">'
 notable_head = '<table class="wrapper-box"><tr><td><span class="bold text-md">'
-inventory_head_rarity = '</div><span class="text-lg bold">Inventory <span class="text-sm emp"> - Inflation:</span>' \
-                        '</span><table style="width:100%;" class="inventory-table"><tr><th style="text-align:left;">' \
-                        'Item</th><th style="text-align:left;">Cost</th><th style="text-align:left;">Rarity</th></tr>'
-inventory_head_type = '</div><span class="text-lg bold">Inventory <span class="text-sm emp"> - Inflation:</span>' \
-                      '</span><table style="width:100%;" class="inventory-table"><tr><th style="text-align:left;">' \
-                      'Item</th><th style="text-align:left;">Cost</th><th style="text-align:left;">Type</th></tr>'
+inventory_head_rarity = '</div><span class="text-lg bold">Inventory <span class="text-sm emp"> - Inflation:</span></sp'\
+                        'an><table style="width:100%;" class="inventory-table"><tr><th style="text-align:left;">Item</'\
+                        'th><th style="text-align:left;">Cost</th><th style="text-align:left;">Rarity</th></tr>'
+inventory_head_type = '</div><span class="text-lg bold">Inventory <span class="text-sm emp"> - Inflation:</span></span'\
+                      '><table style="width:100%;" class="inventory-table"><tr><th style="text-align:left;">Item</th><'\
+                      'th style="text-align:left;">Cost</th><th style="text-align:left;">Type</th></tr>'
 characters = positions = []
 Notable = False
 
