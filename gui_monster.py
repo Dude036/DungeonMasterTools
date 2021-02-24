0582a91111c1ee09032432b9430ece62e541c712
Joshua Higham <josh@higham.com>
2021-02-24 05:45:05 -0700

    Algorithm Optimization

--- gui_monster.py
+++ gui_monster.py
@@ -11,6 +11,8 @@ Stats = {}
 def autofill_text(text):
     global Names
     p = len(text)
+    if p == 0:
+        return []
     s = 0
     e = len(Names) - 1
     # Binary Search based on starts with
@@ -20,7 +22,7 @@ def autofill_text(text):
         if Names[m][:p].lower() < text.lower():
             s = m + 1
         elif Names[m][:p].lower() > text.lower():
-            e = m + 1
+            e = m - 1
         else:
             print("Breaking")
             break
